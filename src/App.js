b5cdc878c1378d68f278a43cbdb4333782c1ccd0
Jordan Davidson <jordandavidson1990@hotmail.com>
2020-02-26 11:04:16 +0000

    displays the 16 cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Board from './components/board';
+import initializeDeck from './deck';
 import './App.css';
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+
+  useEffect(() => {
+    setCards(initializeDeck())
+  }, [])
+
+  const handleClick = (id) => {
+    setFlipped([...flipped, id])
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Can you remember where the cards are?</h2>
+      <Board
+        cards={cards}
+        flipped={flipped}
+        handleClick={handleClick} />
     </div>
   );
 }
